4ae21921e049a3540efdbc47b012b926f7ea4c58
Chami <piro@live.com>
2023-10-16 18:17:44 -0500

    Create YoutubeAntiAntiAdblock.user.js

---
+++ YoutubeAntiAntiAdblock.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Youtube Embedder
+// @namespace    http://github.com/planetarian/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Chami
+// @match        https://www.youtube.com/watch*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function youtube_parser(url){
+        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+        var match = url.match(regExp);
+        return (match&&match[7].length==11)? match[7] : false;
+    }
+
+    const pageManager = document.getElementById('page-manager');
+    const pageObserver = new MutationObserver(pageMutated);
+    pageObserver.observe(pageManager, { childList: true });
+
+    var modified = false;
+
+    function pageMutated(mutationList, observer) {
+        if (modified) return;
+        console.log("Mutation observed.")
+        for (const mutation of mutationList) {
+            if (mutation.type === "childList") {
+                const playerContainer = document.getElementById('player');
+                if (!playerContainer) continue;
+
+                // remove the notice
+                const errorOverlay = document.getElementById('error-screen');
+                errorOverlay.parentElement.removeChild(errorOverlay);
+
+                // replace the player
+                const player = document.getElementById('ytd-player');
+                const videoId = youtube_parser(document.location.href);
+                player.innerHTML = `<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${videoId}"></iframe>`;
+                modified = true;
+            }
+        }
+    }
+})();
