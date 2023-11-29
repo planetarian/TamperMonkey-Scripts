aef87c96777b19eb639fd6aa94c55d57ee7ab348
Chami <piro@live.com>
2023-11-29 12:48:02 -0600

    Remove bing rebates cashback text

--- BingRebatesAntiClickjack.user.js
+++ BingRebatesAntiClickjack.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bing Rebates Anti-Clickjack
 // @namespace    http://github.com/planetarian
-// @version      0.1
+// @version      0.2
 // @description  Removes Bing Rebates jdoqocy.com clickjacking
 // @author       Chami
 // @match        https://rewards.bing.com/*
@@ -9,12 +9,16 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
 // @updateURL    https://github.com/planetarian/TamperMonkey-Scripts/raw/main/BingRebatesAntiClickjack.user.js
 // @downloadURL  https://github.com/planetarian/TamperMonkey-Scripts/raw/main/BingRebatesAntiClickjack.user.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    // Remove Bing Rebates 'Up to X% cash back' text from results
+    GM_addStyle(".rebateContainer { display: none !important; }");
+
+    // Sanitize clickjacked URLs
     const srcs = document.links;
     for (const src of srcs) {
         var url = src.href;
@@ -33,6 +37,4 @@
             console.error(err);
         }
     }
-
-    // Your code here...
 })();
